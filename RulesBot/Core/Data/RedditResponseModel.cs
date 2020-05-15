6e81efd1d1581f1fc8c576e7d90114e641753685
Máté Farkas <f.mate1128@gmail.com>
2020-05-15 23:36:44 +0200

    Added Reddit integration, removed help command

---
+++ RulesBot/Core/Data/RedditResponseModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace RulesBot.Core.Data
+{
+    public class RedditResponseModel
+    {
+        public RedditResponseInner Data { get; set; }
+        public class RedditResponseInner
+        {
+            public IEnumerable<SubredditData> Children { get; set; }
+            public class SubredditData
+            {
+                public SubredditDataInner Data { get; set; }
+                public class SubredditDataInner
+                {
+                    public string Title { get; set; }
+                    public string Permalink { get; set; }
+                    public int Score { get; set; }
+                    public bool Stickied { get; set; }
+
+                }
+            }
+        }
+    }
+}
