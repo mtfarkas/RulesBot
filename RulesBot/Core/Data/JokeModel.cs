41b2995ed56909659a538e0caf499d443795d782
Máté Farkas <f.mate1128@gmail.com>
2020-05-15 12:26:16 +0200

    Added !joke command

---
+++ RulesBot/Core/Data/JokeModel.cs
@@ -0,0 +1,15 @@
+﻿namespace RulesBot.Core.Data
+{
+    public class JokeModel
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Setup { get; set; }
+        public string Punchline { get; set; }
+
+        public string Complete
+        {
+            get => $"{Setup}\n{Punchline}";
+        }
+    }
+}
