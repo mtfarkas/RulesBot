bc3ea8404d374b0c78c928c4aae310b40269c98b
Máté Farkas <f.mate1128@gmail.com>
2020-05-15 12:19:41 +0200

    Ping Module

---
+++ RulesBot/Commands/PingModule.cs
@@ -0,0 +1,12 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace RulesBot.Commands
+{
+    public class PingModule: ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public Task Pong()
+            => Context.Channel.SendMessageAsync("pong!");
+    }
+}
